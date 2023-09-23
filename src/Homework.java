0557e9dd754b89cb1dbe849df7f229d8b6bf3db3
Роман <urmatos92@gmai.com>
2023-09-23 19:28:52 +0700

    ДЗ методы bild v .2

--- src/Homework.java
+++ src/Homework.java
@@ -3,40 +3,54 @@ import java.time.LocalDate;
 public class Homework {
     public static void main(String[] args) {
         int year = 2024;
-        task1(year);
+        leapYear(year);
         int clientOC = 0;
-        int clientDeviceYear = LocalDate.now().getYear();
-        task2(clientOC,clientDeviceYear);
-        int deliveryDistance = 71;
-        task3(deliveryDistance);
+        int clientDeviceYear = 2022;
+        clienApp(clientOC, clientDeviceYear);
+        int deliveryDistance = 0;
+        cardDelivery(deliveryDistance);
+
+        periodDelivery(cardDelivery(deliveryDistance));
 
     }
 
-    public static void task1(int year) {
+    public static void leapYear(int year) {
         if ((year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year > 1584) {
-            System.out.println(year+" - високосный год");
+            System.out.println(year + " - високосный год");
         } else {
-            System.out.println(year+" -не високосный год ");
+            System.out.println(year + " - не високосный год");
         }
     }
-    public static void task2(int clientOC, int clientDeviceYear){
-        if (clientOC == 0 && clientDeviceYear >= 2015) {
+
+    public static void clienApp(int clientOC, int clientDeviceYear) {
+        if (clientOC == 0 && clientDeviceYear >= LocalDate.now().getYear()) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOC == 0 && clientDeviceYear <= 2015) {
+        } else if (clientOC == 0 && clientDeviceYear <= LocalDate.now().getYear()) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOC == 1 && clientDeviceYear >= 2015) {
+        } else if (clientOC == 1 && clientDeviceYear >= LocalDate.now().getYear()) {
             System.out.println("Установите версию приложения для Android по ссылке");
         } else {
             System.out.println("Установите облегченную версию приложения для Android по ссылке.");
         }
     }
 
-    public static void task3(int deliveryDistance){
+    public static int cardDelivery(int deliveryDistance) {
         if (deliveryDistance < 20) {
-            System.out.println("Доставка будет осуществлена в течение суток");
+            return 1;
         } else if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-            System.out.println("Доставка будет осуществлена в течение 2 дней");
+            return 2;
         } else if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+    public static void periodDelivery (int deliveryTime){
+        if (deliveryTime == 1){
+            System.out.println("Доставка будет осуществлена в течение суток");
+        } else if (deliveryTime == 2) {
+            System.out.println("Доставка будет осуществлена в течение 2 дней");
+        } else if (deliveryTime == 3) {
             System.out.println("Доставка будет осуществлена в течение 3 дней");
         } else {
             System.out.println("Ваше расположение не входит в зону обслуживания нашей компании.");
